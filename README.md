0e18967dd9ca3e6314d072eef26e779e8f2e70dc
combowomb0 <jonick9009@gmail.com>
2019-08-14 21:35:13 +0400

    initial

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# altv-notification-system
+# altv-notification-system  
+usage:  
+```js
+webView.emit('notify:send', {
+  text: text,
+  timeout: 5000,
+  textColor: '#000000',
+  backgroundColor: 'rgba(236,236,255,0.85)',
+  lineColor: '#6c7ae0'
+});
+```
---
+++ resources/notify/client/client.mjs
@@ -0,0 +1,26 @@
+import alt from 'alt';
+
+const notify = {
+  isLoaded: false,
+  view: null
+};
+
+alt.on('connectionComplete', () => {
+  notify.view = new alt.WebView('http://resources/notify/client/view/index.html');
+  notify.view.on('notify:loaded', () => {
+    notify.isLoaded = true;
+  });
+});
+
+alt.on('consoleCommand', (command, ...args) => {
+  if(command === 'notify' && notify.isLoaded) {
+    const text = args.join(' ');
+    notify.view.emit('notify:send', {
+      text: text,
+      timeout: 5000,
+      textColor: '#000000',
+      backgroundColor: 'rgba(236,236,255,0.85)',
+      lineColor: '#6c7ae0'
+    });
+  }
+});
